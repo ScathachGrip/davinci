034f9391fc15126c1ef1ff539a16727f6a75917b
SNEK WAIFU <bibsindra@gmail.com>
2026-07-28 16:15:49 +0700

    chore(release): 1.1.8-alpha

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 // AppVersion defines the current version of the application.
-var AppVersion = "1.1.7-alpha"
+var AppVersion = "1.1.8-alpha"
 
 func main() {
 	// Load .env file if it exists (for local development)
